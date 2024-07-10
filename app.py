94de7f76d952f71843d6ffd0b893d98c7dec4013
Noor Khan <mohdnoor.khan@sscinc.com>
2024-07-10 12:55:53 +0530

    Updated api to be restful. Added translation code

--- app.py
+++ app.py
@@ -20,41 +20,33 @@ current_model_name = "base"
 model = load_whisper_model(current_model_name)
 options = whisper.DecodingOptions(language="en")
 
-@app.route('/transcribe', methods=['POST'])
-def transcribe():
+@app.route('/api/transcription', methods=['POST'])
+def create_transcription():
     if 'audio' not in request.files:
         return jsonify({'error': 'No audio file provided'}), 400
 
     audio_file = request.files['audio']
-
-    # Save the file to a temporary location
     file_path = f"./{audio_file.filename}"
     audio_file.save(file_path)
-
-    # Transcribe the audio file using Whisper
-    result = model.transcribe(file_path)
-
-    # Extract only the transcribed text
+    result = model.transcribe(file_path, language="en", task="translate",fp16=False)
     transcribed_text = result.get('text', '')
-
-    # Remove the temporary file
     os.remove(file_path)
 
     return jsonify({'transcribed_text': transcribed_text})
 
-@app.route('/select_model', methods=['POST'])
-def select_model():
+@app.route('/api/model', methods=['POST'])
+def update_model():
     global model, current_model_name
     model_name = request.json.get('model_name', 'base')
     model = load_whisper_model(model_name)
     current_model_name = model_name
     return jsonify({'message': f'Model switched to {model_name}'}), 200
 
-@app.route('/get_model', methods=['GET'])
-def get_model():
+@app.route('/api/model', methods=['GET'])
+def get_current_model():
     return jsonify({'current_model': current_model_name})
 
-@app.route('/test', methods=['GET'])
+@app.route('/api/test', methods=['GET'])
 def test():
     return jsonify({'message': 'API is working!'})
 
